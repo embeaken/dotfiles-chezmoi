e565ebc8de81a4f21ac6abb4a01c7370fa379540
Ilya Rubnich <ilya@rubni.ch>
2025-07-25 21:46:47 -0400

    pager

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,9 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+# shared
 set -x SSH_AUTH_SOCK $HOME/.1password/agent.sock
+set -x EDITOR nvim
+set -x PAGER less
 
 set __fish_git_prompt_showdirtystate 'yes'
 
